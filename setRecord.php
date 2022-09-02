668e76814c495858dad5cc6775061935c81d7f44
ladparis <laurent@dubertrand.fr>
2022-09-02 18:07:43 +0200

    setRecord.php incremental

--- setRecord.php
+++ setRecord.php
@@ -7,15 +7,22 @@ if(isset($_REQUEST["like"]))
     $like = $_REQUEST["like"];
 $mysqli = new mysqli("localhost", "laurent", "1124Da", "gallery");
 $result = $mysqli->query("SELECT likes, dislikes FROM records WHERE filename = '".$filename."';");
-$result->data_seek(0);
-$row = $result->fetch_assoc();
-$likes = $row["likes"];
-$dislikes = $row["dislikes"];
-if ($like == "yes")
-    $likes++;
-if ($like == "no")
-    $dislikes++;
-
-$mysqli->query("UPDATE records SET likes = ".$likes." , dislikes = ".$dislikes." WHERE filename = '".$filename."';");
+if($result->num_rows !=0){
+    $row = $result->fetch_assoc();
+    $likes = $row["likes"];
+    $dislikes = $row["dislikes"];
+    if ($like == "yes")
+        $likes++;
+    if ($like == "no")
+        $dislikes++;
+    
+    $mysqli->query("UPDATE records SET likes = ".$likes." , dislikes = ".$dislikes." WHERE filename = '".$filename."';");
+}
+else{
+    if($like=="yes")
+        $mysqli->query("INSERT INTO records VALUES('".$filename."',1,0)");
+    else if($like=="no")
+        $mysqli->query("INSERT INTO records VALUES('".$filename."',0,1)");
+}
 $mysqli->close();
 ?>
