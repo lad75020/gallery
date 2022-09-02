720225bfe52aacf09f7d119f7dddf729c34a4ac9
ladparis <laurent@dubertrand.fr>
2022-09-03 01:28:30 +0200

    Update videostream.php

--- videostream.php
+++ videostream.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Description of VideoStream
- *
- * @author Rana
- * @link http://codesamplez.com/programming/php-html5-video-streaming-tutorial
- */
 class VideoStream
 {
     private $path = "";
