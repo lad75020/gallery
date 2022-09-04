57c138e1997007e6617944e61f0c04125474ce5f
ladparis <laurent@dubertrand.fr>
2022-09-04 13:16:41 +0200

    removing stuff from git

--- getRandomInt8Array.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $random_number_array = range(0, 255);
-    shuffle($random_number_array );
-    $random_number_array = array_slice($random_number_array ,0,$_REQUEST['size']-1);
-    echo(json_encode( $random_number_array));
-?>
