3e75650962d243f7fbfe77c91ae6c12199e574e2
Laurent Dubertrand <laurent@dubertrand.fr>
2022-12-13 21:42:46 +0100

    valid

--- deleteFile.php
+++ deleteFile.php
@@ -1,7 +1,7 @@
 <?php
 require("config.inc.php");
-$path = isset($_REQUEST["filename"])?$_REQUEST["filename"]:"";
-$pwd = urldecode(isset($_REQUEST["pwd"])?$_REQUEST["pwd"]:"");
+$path = isset($_REQUEST["filename"])?urldecode($_REQUEST["filename"]):"";
+$pwd = isset($_REQUEST["pwd"])?urldecode($_REQUEST["pwd"]):"");
 
 if ($pwd == __ADMIN_PASSWORD__ && $path != ""){
 	if (!unlink(__SMALL_PIX_FULL_PATH__.$path))
--- getVideo.php
+++ getVideo.php
@@ -1,7 +1,7 @@
 <?php
 require("config.inc.php");
 $path = __VIDEOS_FULL_PATH__;
-$path .= urldecode((isset($_REQUEST["filename"]))? $_REQUEST["filename"]:"");
+$path .= isset($_REQUEST["filename"])? urldecode($_REQUEST["filename"]):"");
 include "./videostream.php";
 $stream = new VideoStream($path);
 $stream->start();
