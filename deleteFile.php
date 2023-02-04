3c4f4c27914d80c7d4f651d191d19fe641ebbae2
Laurent Dubertrand <laurent@dubertrand.fr>
2023-02-04 17:13:32 +0100

    sort.html flags

--- deleteFile.php
+++ deleteFile.php
@@ -1,15 +1,22 @@
 <?php
 require("config.inc.php");
-$path = isset($_REQUEST["filename"])?urldecode($_REQUEST["filename"]):"";
-$pwd = isset($_REQUEST["pwd"])?urldecode($_REQUEST["pwd"]):"");
-
-if ($pwd == __ADMIN_PASSWORD__ && $path != ""){
-	if (!unlink(__SMALL_PIX_FULL_PATH__.$path))
-		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-	else{
-		$mysqli = new mysqli("localhost", __DB_USER__, __DB_PASSWORD__, __DB_NAME__);
-		$mysqli->query( "DELETE FROM records WHERE filename  = '".$mysqli->real_escape_string($path)."';");
-		$mysqli->close();
+$path = isset($_REQUEST["filename"])?$_REQUEST["filename"]:"";
+$folder = isset($_REQUEST["folder"])?$_REQUEST["folder"]:__SMALL_PIX_FOLDER__;
+if($_SERVER["HTTP_HOST"] == __ADMIN_HOST__){
+	if ($path != ""){
+		if(!unlink(__FOLDERS_ROOT__.$folder.'/'.$path))
+			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+		else{
+			if ($folder == __SMALL_PIX_FOLDER__){
+				unlink(__ORIGINAL_PIX_FULL_PATH__.$path);
+				$mysqli = new mysqli("localhost", __DB_USER__, __DB_PASSWORD__, __DB_NAME__);
+				$mysqli->query("DELETE FROM records WHERE filename  = '".$mysqli->real_escape_string($path)."';");
+				$mysqli->close();
+				apcu_delete("imgdir");
+			}
+		}
 	}
 }
+else
+	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Not Allowed', true, 403);
 ?>
--- js/sort3.js
+++ js/sort3.js
@@ -1,108 +1,126 @@
-		var mdp = "";
-		function drawPix(){
-			xmlhttp2 = new XMLHttpRequest();
-			xmlhttp2.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					files = this.responseText.replace("\r\n", "").split(";");
-					for (var i=0;i<files.length;i++){
-						if (files[i]!=""){
-							var oImg=document.createElement("IMG");
-							oImg.setAttribute("class","thumbnails");
-							oImg.setAttribute("id","drag"+i);
-							oImg.setAttribute("height","100px");
-							oImg.setAttribute("draggable","true");
-							oImg.setAttribute("onclick","moveFile(this,'"+files[i]+"');");
-							oImg.setAttribute("onmouseover","showImg(this)");
-							oImg.setAttribute("src","/uploads/"+files[i]);
-							//oImg.setAttribute("ondragstart","drag('event')");
-							document.getElementById("divSource").appendChild(oImg);
-						}
-					}
+var draggedElement = null;
+function drawPix(){
+	xmlhttp2 = new XMLHttpRequest();
+	xmlhttp2.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			files = this.responseText.replace("\r\n", "").split(";");
+			for (var i=0;i<files.length;i++){
+				if (files[i]!=""){
+					var oImg = document.createElement("IMG");
+					oImg.setAttribute("class","thumbnails");
+					oImg.setAttribute("id","drag"+i);
+					oImg.setAttribute("height","100px");
+					oImg.setAttribute("draggable","true");
+					oImg.setAttribute("ondrag","draggedElement=this.parentElement")
+					oImg.setAttribute("onclick","moveFile(this.parentElement,'"+files[i]+"');");
+                    oImg.setAttribute("onmouseover","showImg(this)");
+					oImg.setAttribute("src","/uploads/"+files[i]);
+					var oDiv = document.createElement("DIV");
+					oDiv.setAttribute("class" , "smallimage");
+					oDiv.setAttribute("id" , "span"+i);
+					var oX = document.createElement("DIV");
+					oX.setAttribute("class","flagDelete");
+					oX.setAttribute("onclick","deleteFile('"+ oImg.src.split('/').pop() + "',this.parentElement);");
+					oX.setAttribute("style","z-index:200");
+					oX.setAttribute("title", "click to delete");
+					oX.innerHTML = "&#128681;";
+					oDiv.appendChild(oX);
+					oDiv.appendChild(oImg);
+					document.getElementById("divSource").appendChild(oDiv);
 				}
-			};
-			xmlhttp2.open("GET", "getFIleList.php?cached=no&folder=uploads&apcukey=uploaddir",true);
-			xmlhttp2.send();
-		}
-		function allowDrop(ev) {
-		  ev.preventDefault();
-		}
-		function drag(ev) {
-		  ev.dataTransfer.setData("text", ev.target.id);
-		}
-		function showImg(eImg){
-			let oImg = document.createElement("IMG");
-			oImg.setAttribute("src", eImg.src);
-			
-			if(document.getElementById("viewer")){
-				document.getElementById("viewer").replaceChild(oImg, document.getElementById("viewer").firstChild);
-				document.getElementById("viewer").style.display='block';
 			}
-			else{
-				let oDiv = document.createElement("DIV");
-				oDiv.setAttribute("style", "z-index:500;position:fixed;display:block;top:10px;left:640px;cursor:pointer");
-				oDiv.setAttribute("onclick","this.style.display='none'");
-				oDiv.setAttribute("id","viewer");
-				oDiv.appendChild(oImg);
-				document.body.appendChild(oDiv);
-			}       
 		}
-		function moveFile(obj,filename) {
-		
-			var xmlhttp = new XMLHttpRequest();
-		
-			xmlhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200){
-					obj.remove();
-				}
-				else if(this.readyState == 4 && this.status == 403){
-					alert( "Not Allowed " + this.responseText);
-				}
-				else if(this.readyState == 4 && this.status == 500){
-					alert( "System Error: " + this.responseText);
-				}	
-			}
-			xmlhttp.open("POST", "move.php", true);
-			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xmlhttp.send("path="+encodeURI(filename));
-		}
+	};
+	xmlhttp2.open("GET", "getFIleList.php?cached=no&folder=uploads&apcukey=uploaddir",true);
+	xmlhttp2.send();
+}
 
-		function move(ev) {
-			ev.preventDefault();
-			var xmlhttp = new XMLHttpRequest();
-		
-			xmlhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200){
-					alert("Move success! " + this.responseText);
-					location.href = "sort.html";
-				}
-				else if(this.readyState == 4 && this.status == 403){
-					alert( "Not Allowed " + this.responseText);
-				}
-				else if(this.readyState == 4 && this.status == 500){
-					alert( "System Error: " + this.responseText);
-				}	
-			}
-			xmlhttp.open("POST", "move.php", true);
-			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xmlhttp.send("path="+ev.dataTransfer.getData("text").split("/").pop());
+function  deleteFile(filename, oDiv){
+    xmlhttp5 = new XMLHttpRequest();
+    xmlhttp5.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+			
+            oDiv.remove();
+        } 
+    };
+    xmlhttp5.open("POST", "deleteFile.php",true);
+    xmlhttp5.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp5.send("filename="+filename+"&folder=uploads");
+}
+function move(ev) {
+	ev.preventDefault();
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			draggedElement.remove();
+			//location.href = "sort.html";
 		}
-		function erase(ev) {
-			ev.preventDefault();
-			var xmlhttp2 = new XMLHttpRequest();
-		
-			xmlhttp2.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200){
-					alert( "Delete success: " + this.responseText);
-					location.href = "sort.html";}
-				else if(this.readyState == 4 && this.status == 403){
-					alert( "Not allowed " + this.responseText);
-				}
-				else if(this.readyState == 4 && this.status == 500){
-					alert( "System Error: " + this.responseText);
-				}		
-			}
-				
-			xmlhttp2.open("POST", "delete.php", true);
-			xmlhttp2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xmlhttp2.send("path="+ev.dataTransfer.getData("text").split("/").pop());
+		else if(this.readyState == 4 && this.status == 403){
+			alert( "Not Allowed " + this.responseText);
 		}
+		else if(this.readyState == 4 && this.status == 500){
+			alert( "System Error: " + this.responseText);
+		}	
+	}
+	xmlhttp.open("POST", "move.php", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send("path="+ev.dataTransfer.getData("text").split("/").pop());
+}
+function showImg(eImg){
+    let oImg = document.createElement("IMG");
+    oImg.setAttribute("src", eImg.src);
+    
+    if(document.getElementById("viewer")){
+        document.getElementById("viewer").replaceChild(oImg, document.getElementById("viewer").firstChild);
+        document.getElementById("viewer").style.display='block';
+    }
+    else{
+        let oDiv = document.createElement("DIV");
+        oDiv.setAttribute("style", "z-index:500;position:fixed;display:block;top:10px;left:640px;cursor:pointer");
+        oDiv.setAttribute("onclick","this.style.display='none'");
+        oDiv.setAttribute("id","viewer");
+        oDiv.appendChild(oImg);
+        document.body.appendChild(oDiv);
+    }       
+}
+function moveFile(obj,filename) {
+
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+            obj.remove();
+		}
+		else if(this.readyState == 4 && this.status == 403){
+			alert( "Not Allowed " + this.responseText);
+		}
+		else if(this.readyState == 4 && this.status == 500){
+			alert( "System Error: " + this.responseText);
+		}	
+	}
+	xmlhttp.open("POST", "move.php", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send("path="+encodeURI(filename));
+}
+
+function erase(ev) {
+	ev.preventDefault();
+	var xmlhttp2 = new XMLHttpRequest();
+	//alert(ev.dataTransfer.getData("text"));
+	xmlhttp2.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			draggedElement.remove();
+		}
+		else if(this.readyState == 4 && this.status == 403){
+			alert( "Not allowed " + this.responseText);
+		}
+		else if(this.readyState == 4 && this.status == 500){
+			alert( "System Error: " + this.responseText);
+		}		
+	}
+		
+	xmlhttp2.open("POST", "delete.php", true);
+	xmlhttp2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp2.send("path="+ev.dataTransfer.getData("text").split("/").pop());
+}
